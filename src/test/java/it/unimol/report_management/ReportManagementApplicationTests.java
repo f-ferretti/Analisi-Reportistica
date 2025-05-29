82d5c6cb948725521c4df1671bd26987bebd2e26
f-ferretti <francesco.ferrettissimo@gmail.com>
2025-05-29 15:14:55 +0200

    setup di spring boot

---
+++ src/test/java/it/unimol/report_management/ReportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimol.report_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
