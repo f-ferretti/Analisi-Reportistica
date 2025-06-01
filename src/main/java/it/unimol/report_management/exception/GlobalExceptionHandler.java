8a4665b3c231f12cd029f27fd7572449192b3a29
f-ferretti <francesco.ferrettissimo@gmail.com>
2025-06-01 16:56:28 +0200

    aggiunta errori globali

---
+++ src/main/java/it/unimol/report_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package it.unimol.report_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(
+                Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Bad Request",
+                        "message", ex.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneric(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Internal Server Error",
+                        "message", ex.getMessage()
+                )
+        );
+    }
+}
